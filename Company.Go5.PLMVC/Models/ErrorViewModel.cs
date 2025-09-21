201e7882a5586669a3607f464b8be328d9e6829e
ehab elhelw <popehab057@gmail.com>
2025-09-21 06:19:59 +0300

    Add project files.

---
+++ Company.Go5.PLMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Company.Go5.PLMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
