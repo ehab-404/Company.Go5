978c2dd9570edd7573861ed0c78cc9ade2664d32
ehab elhelw <popehab057@gmail.com>
2025-09-23 00:23:10 +0300

    Department module bbl

---
+++ Company.Go5.BLL/Interfaces/IDepartmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Go5.DAL.Models;
+
+namespace Company.Go5.BLL.Interfaces
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Department>? GetAll();
+        Department? GetById(int id);
+
+        int Add(Department department); 
+        int Update(Department department);
+        int Delete(Department department);
+
+
+
+
+    }
+}
