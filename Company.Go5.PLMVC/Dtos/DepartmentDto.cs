11b016538409151f8e062dfd806b5bc8b7ceca27
ehab elhelw <popehab057@gmail.com>
2025-09-25 01:36:36 +0300

    department details

--- Company.Go5.PLMVC/Dtos/DepartmentDto.cs
+++ Company.Go5.PLMVC/Dtos/DepartmentDto.cs
@@ -6,6 +6,7 @@ namespace Company.Go5.PLMVC.Dtos
     {
 
         //client side validation 
+        [Required(ErrorMessage = "code is required ")]
         [Required(ErrorMessage ="code is required ")]
         public string Code { get; set; }
 
