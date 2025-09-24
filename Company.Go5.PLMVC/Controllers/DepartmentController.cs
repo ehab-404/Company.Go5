db225cd51dd3a23513c11586232c51aaa5d1b800
ehab elhelw <popehab057@gmail.com>
2025-09-24 21:16:55 +0300

    create  Get action that return with creation form that submit to Post action that validate and save then return to index

--- Company.Go5.PLMVC/Controllers/DepartmentController.cs
+++ Company.Go5.PLMVC/Controllers/DepartmentController.cs
@@ -1,28 +1,63 @@
 ﻿using Company.Go5.BLL.Interfaces;
 using Company.Go5.BLL.Repositories;
+using Company.Go5.DAL.Models;
+using Company.Go5.PLMVC.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Go5.PLMVC.Controllers
 {
     public class DepartmentController : Controller
     {
-        private readonly IDepartmentRepository _repository;
+        private readonly IDepartmentRepository _departmentRepository;
 
         //ask clr to create object of DepartmentRepository
         //when assigning it reference of type of IDepartmentRepository
         //by making dependency injection in services region 
-        public DepartmentController(IDepartmentRepository repository) 
+        public DepartmentController(IDepartmentRepository departmentRepository) 
         { 
         
-            _repository = repository;
+            _departmentRepository = departmentRepository;
         }
 
         [HttpGet]  //get : / Department/Index 
         public IActionResult Index()
         {
-            var departments = _repository.GetAll();
+            var departments = _departmentRepository.GetAll();
 
             return View(departments);
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Create(DepartmentDto department)
+        {
+            if(ModelState.IsValid) //server side validation 
+            {
+                //manual mapping 
+                var NewDepartment = new Department
+                {
+                    Code = department.Code,
+                    Name = department.Name,
+                    CreateAt = department.CreateAt
+                };
+                _departmentRepository.Add(NewDepartment);
+
+            }
+
+            
+
+            
+
+            return RedirectToAction(nameof(Index) );
+
+        }
     }
 }
+
---
+++ Company.Go5.PLMVC/Dtos/DepartmentDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Company.Go5.PLMVC.Dtos
+{
+    public class DepartmentDto
+    {
+
+        //client side validation 
+        [Required(ErrorMessage ="code is required ")]
+        public string Code { get; set; }
+
+        [Required(ErrorMessage = "name is required ")]
+
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "create at  is required ")]
+
+        public DateTime CreateAt { get; set; }
+    }
+}
