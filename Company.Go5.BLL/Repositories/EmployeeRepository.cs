6b71d3c1e1589a529cd6f98b4fd007f8aeab20f2
ehab elhelw <popehab057@gmail.com>
2025-09-25 09:10:53 +0300

    employee bll

---
+++ Company.Go5.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Company.Go5.BLL.Interfaces;
+using Company.Go5.DAL.Data.Contexts;
+using Company.Go5.DAL.Models;
+
+namespace Company.Go5.BLL.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly CompanyDbContext _dbContext;
+
+        public EmployeeRepository(CompanyDbContext dbContext)
+        {
+            _dbContext=dbContext;
+        }
+
+        public int Add(Employee employee)
+        {
+            _dbContext.Add(employee);
+           return _dbContext.SaveChanges();
+
+
+
+        }
+
+        public int Delete(Employee employee)
+        {
+            _dbContext.Remove(employee);
+            return _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _dbContext.Employees.ToList();
+        }
+
+        public Employee? GetById(int id)
+        {
+            return _dbContext.Employees.Find(id);
+
+
+
+        }
+
+        public int Update(Employee employee)
+        {
+            _dbContext.Update(employee);
+            return _dbContext.SaveChanges();
+        }
+    }
+}
