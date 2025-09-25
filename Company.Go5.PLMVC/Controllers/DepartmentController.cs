a8c6fe5468c785dda0660730b70bb0a6192379d3
ehab elhelw <popehab057@gmail.com>
2025-09-25 03:53:03 +0300

    edit department

--- Company.Go5.PLMVC/Controllers/DepartmentController.cs
+++ Company.Go5.PLMVC/Controllers/DepartmentController.cs
@@ -2,6 +2,7 @@ using Company.Go5.BLL.Interfaces;
 using Company.Go5.BLL.Repositories;
 using Company.Go5.DAL.Models;
 using Company.Go5.PLMVC.Dtos;
+using Humanizer;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Go5.PLMVC.Controllers
@@ -80,6 +81,122 @@ namespace Company.Go5.PLMVC.Controllers
 
             return View(DepartmentDto);
         }
+
+
+
+
+
+
+        [HttpGet]
+
+        public IActionResult Edit(int? id)
+        {
+            if (id is null) { return BadRequest("id is required"); }
+
+            var department = _departmentRepository.GetById(id.Value);
+
+            if (department is null) { return NotFound($"no department with id {id}"); }
+
+            var DepartmentDto = new DepartmentDto()
+            {
+                Code = department.Code,
+                Name = department.Name,
+                CreateAt = department.CreateAt
+
+
+            };
+
+            return View(DepartmentDto);
+
+
+
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit( DepartmentDto DepartmentDto ,[FromRoute] int id)
+        {
+
+
+
+            if (!ModelState.IsValid)
+            {
+                return View(DepartmentDto); 
+            }
+
+
+            var department = _departmentRepository.GetById(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            department.Name= DepartmentDto.Name;
+            department.Code = DepartmentDto.Code;
+            department.CreateAt= DepartmentDto.CreateAt;
+
+
+
+            var count = _departmentRepository.Update(department);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+
+                }
+         
+
+            return View(DepartmentDto);
+
+
+        }
+
+
+
+        //[HttpGet]
+
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id is null) { return BadRequest("id is required"); }
+
+        //    var department = _departmentRepository.GetById(id.Value);
+
+        //    if (department is null) { return NotFound($"no department with id {id}"); }
+
+
+
+        //    return View(department);
+
+
+
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit([FromRoute] int id, Department department)
+        //{
+
+
+
+        //    if (ModelState.IsValid)
+        //    {
+
+
+        //        var count = _departmentRepository.Update(department);
+        //        if (count > 0)
+        //        {
+        //            return RedirectToAction(nameof(Index));
+
+        //        }
+        //    }
+
+        //    return View(department);
+
+        //}
     }
 }
 
